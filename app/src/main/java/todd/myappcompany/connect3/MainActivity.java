983115498176028851ac9075f933f17115875a24
Torrence Cole <trcole@gmail.com>
2021-03-21 20:41:58 -0400

    finished game logic

--- app/src/main/java/todd/myappcompany/connect3/MainActivity.java
+++ app/src/main/java/todd/myappcompany/connect3/MainActivity.java
@@ -5,20 +5,94 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import androidx.gridlayout.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    public void dropIn(View view) {
-        Log.i("info", "Button Clicked");
+    // 0: yellow, 1: red
+    int activePlayer = 0;
 
+    int[] gameState = {2,2,2,2,2,2,2,2,2};
+
+    int[][] winningPosition = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+
+    boolean gameActive = true;
+
+    public void dropIn(View view) {
         ImageView counter = (ImageView) view;
 
-        counter.setTranslationY(-1500);
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
 
-        counter.setImageResource(R.drawable.yellow);
+        if(gameState[tappedCounter] == 2 && gameActive) {
 
-        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1500);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+            for (int[] winningPosition : winningPosition) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                    // Someone has won!
+                    String winner = "";
+
+                    if (activePlayer == 1) {
+                        winner = "Yellow";
+                    } else {
+                        winner = "Red";
+                    }
+
+                    Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+                    TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+                    winnerTextView.setText(winner + " has won");
+
+                    playAgainButton.setVisibility(View.VISIBLE);
+                    winnerTextView.setVisibility(View.VISIBLE);
+
+
+                    gameActive = false;
+                }
+            }
+        }
+    }
+
+    public void playAgain(View view) {
+        Button playAgainButton = (Button) findViewById(R.id.playAgainButton);
+        TextView winnerTextView = (TextView) findViewById(R.id.winnerTextView);
+
+        playAgainButton.setVisibility(View.INVISIBLE);
+        winnerTextView.setVisibility(View.INVISIBLE);
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+
+        for (int i=0; i<gridLayout.getChildCount(); i++) {
+            ImageView counter = (ImageView) gridLayout.getChildAt(i);
+
+            counter.setImageDrawable(null);
+        }
+
+        for (int i=0; i< gameState.length; i++) {
+            gameState[i] = 2;
+        }
+
+        activePlayer = 0;
+
+        gameActive = true;
     }
 
     @Override
